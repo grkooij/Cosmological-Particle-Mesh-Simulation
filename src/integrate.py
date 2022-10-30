fcaad50b14dd49424f065b9ebb0db73c29f7da95
grkooij <grkooij@github.com>
2022-10-30 21:03:55 +0100

    major refactoring. Ram usage more than halved

--- src/integrate.py
+++ src/integrate.py
@@ -3,16 +3,15 @@ import numpy as np
 def integrate(box, x_dat, y_dat, z_dat, vx_dat, vy_dat, vz_dat, a_val, f_a, f_a1, da, potentials):
 	#This function calculates the momenta from the potential field
 	#and updates particle position and momentum
+	directions = [0,1,2]
 
-	#Unpack variables
-	Ngrid = box.Ngrid
-	Lx = box.Lx
+	x_dat, vx_dat = sweep_one_direction(box, potentials, x_dat, y_dat, z_dat, vx_dat, vy_dat, vz_dat, a_val, f_a1, da, directions[0])
+	y_dat, vy_dat = sweep_one_direction(box, potentials, x_dat, y_dat, z_dat, vx_dat, vy_dat, vz_dat, a_val, f_a1, da, directions[1])
+	z_dat, vz_dat = sweep_one_direction(box, potentials, x_dat, y_dat, z_dat, vx_dat, vy_dat, vz_dat, a_val, f_a1, da, directions[2])
 
-	#We again define the cell center coordinates for CIC interpolation
-	x = np.floor(x_dat).astype(int)
-	y = np.floor(y_dat).astype(int)
-	z = np.floor(z_dat).astype(int)
-	
+	return x_dat, y_dat, z_dat, vx_dat, vy_dat, vz_dat
+
+def weights(x, y, z, x_dat, y_dat, z_dat):
 	d_x = x_dat - x
 	d_y = y_dat - y
 	d_z = z_dat - z
@@ -20,69 +19,57 @@ def integrate(box, x_dat, y_dat, z_dat, vx_dat, vy_dat, vz_dat, a_val, f_a, f_a1
 	t_x = 1 - d_x
 	t_y = 1 - d_y
 	t_z = 1 - d_z
+	return np.array([t_x*t_y*t_z, d_x*t_y*t_z, t_x*d_y*t_z, t_x*t_y*d_z, d_x*d_y*t_z, d_x*t_y*d_z, t_x*d_y*d_z, d_x*d_y*d_z])
+	
+def sweep_one_direction(box, potentials, x_dat, y_dat, z_dat, vx, vy, vz, a_val, f_a1, da, direction):
+	Ngrid = box.Ngrid
 
-	#And enforce periodicity for cell center +1/ +2
+	#We again define the cell center coordinates for CIC interpolation
+	x = np.floor(x_dat).astype(int)
+	y = np.floor(y_dat).astype(int)
+	z = np.floor(z_dat).astype(int)
+	t = weights(x, y, z, x_dat, y_dat, z_dat)
+
+	x2 = x; y2 = y; z2 = z
+
+	if direction==0:
+		x = (x+1)%Ngrid
+		x2 = x-1
+		velocities = vx
+		positions = x_dat
+	elif direction==1:
+		y = (y+1)%Ngrid
+		y2 = y-1
+		velocities = vy
+		positions = y_dat
+	else:
+		z = (z+1)%Ngrid
+		z2 = z-1
+		velocities = vz
+		positions = z_dat
+
 	X = (x+1)%Ngrid
 	Y = (y+1)%Ngrid
 	Z = (z+1)%Ngrid
-	X2 = (x+2)%Ngrid
-	Y2 = (y+2)%Ngrid
-	Z2 = (z+2)%Ngrid
+	X2 = (x2+1)%Ngrid
+	Y2 = (y2+1)%Ngrid
+	Z2 = (z2+1)%Ngrid
 
-	#Interpolating for the CIC implementation to obtain particle momenta
-
-	#Interpolating for the momenta in x
-	gx = (-potentials[z,y,X] + potentials[z,y,x-1])/2.
-	gx_x = (-potentials[z,y,X2] + potentials[z,y,x])/2.
-	gx_y = (-potentials[z,Y,X] + potentials[z,Y,x-1])/2.
-	gx_z = (-potentials[Z,y,X] + potentials[Z,y,x-1])/2.
-	gx_xy = (-potentials[z,Y,X2] + potentials[z,Y,x])/2.
-	gx_xz = (-potentials[Z,y,X2] + potentials[Z,y,x])/2.
-	gx_yz = (-potentials[Z,Y,X] + potentials[Z,Y,x-1])/2.
-	gx_xyz = (-potentials[Z,Y,X2] + potentials[Z,Y,x])/2.
-
-	#Interpolating for the momenta in y
-	gy = (-potentials[z,Y,x] + potentials[z,y-1,x])/2.
-	gy_x = (-potentials[z,Y,X] + potentials[z,y-1,X])/2.
-	gy_y = (-potentials[z,Y2,x] + potentials[z,y,x])/2.
-	gy_z = (-potentials[Z,Y,x] + potentials[Z,y-1,x])/2.
-	gy_xy = (-potentials[z,Y2,X] + potentials[z,y,X])/2.
-	gy_xz = (-potentials[Z,Y,X] + potentials[Z,y-1,X])/2.
-	gy_yz = (-potentials[Z,Y2,x] + potentials[Z,y,x])/2.
-	gy_xyz = (-potentials[Z,Y2,X] + potentials[Z,y,X])/2.
-
-	#Interpolating for the momenta in z
-	gz = (-potentials[Z,y,x] + potentials[z-1,y,x])/2.
-	gz_x = (-potentials[Z,y,X] + potentials[z-1,y,X])/2.
-	gz_y = (-potentials[Z,Y,x] + potentials[z-1,Y,x])/2.
-	gz_z = (-potentials[Z2,y,x] + potentials[z,y,x])/2.
-	gz_xy = (-potentials[Z,Y,X] + potentials[z-1,Y,X])/2.
-	gz_xz = (-potentials[Z2,y,X] + potentials[z,y,X])/2.
-	gz_yz = (-potentials[Z2,Y,x] + potentials[z,Y,x])/2.
-	gz_xyz = (-potentials[Z2,Y,X] + potentials[z,Y,X])/2.
-
-	#Calculating contribution for each momentum found above
-	t1 = t_x*t_y*t_z
-	t2 = d_x*t_y*t_z
-	t3 = t_x*d_y*t_z
-	t4 = t_x*t_y*d_z
-	t5 = d_x*d_y*t_z
-	t6 = d_x*t_y*d_z
-	t7 = t_x*d_y*d_z
-	t8 = d_x*d_y*d_z
+	g = (-potentials[z,y,x] + potentials[z2,y2,x2])
+	g_x = (-potentials[z,y,X] + potentials[z2,y2,X2])
+	g_y = (-potentials[z,Y,x] + potentials[z2,Y2,x2])
+	g_z = (-potentials[Z,y,x] + potentials[Z2,y2,x2])
+	g_xy = (-potentials[z,Y,X] + potentials[z2,Y2,X2])
+	g_xz = (-potentials[Z,y,X] + potentials[Z2,y2,X2])
+	g_yz = (-potentials[Z,Y,x] + potentials[Z2,Y2,x2])
+	g_xyz = (-potentials[Z,Y,X] + potentials[Z2,Y2,X2])
 	
 	#We calculate the parent momentum using these contributions
-	g_p_x = gx*t1 + gx_x*t2 + gx_y*t3 + gx_z*t4 + gx_xy*t5 + gx_xz*t6 + gx_yz*t7 + gx_xyz*t8
-	g_p_y = gy*t1 + gy_x*t2 + gy_y*t3 + gy_z*t4 + gy_xy*t5 + gy_xz*t6 + gy_yz*t7 + gy_xyz*t8
-	g_p_z = gz*t1 + gz_x*t2 + gz_y*t3 + gz_z*t4 + gz_xy*t5 + gz_xz*t6 + gz_yz*t7 + gz_xyz*t8
-	
-	#Calculate the new velocity at a+da
-	vx_datnew = vx_dat + da*f_a1*g_p_x
-	vy_datnew = vy_dat + da*f_a1*g_p_y
-	vz_datnew = vz_dat + da*f_a1*g_p_z
+	g_p = (g*t[0] + g_x*t[1] + g_y*t[2] + g_z*t[3] + g_xy*t[4] + g_xz*t[5] + g_yz*t[6] + g_xyz*t[7])/2.
 
-	x_dat = (x_dat + da*vx_datnew/(a_val+da)**2*f_a1) % Ngrid 
-	y_dat = (y_dat + da*vy_datnew/(a_val+da)**2*f_a1) % Ngrid
-	z_dat = (z_dat + da*vz_datnew/(a_val+da)**2*f_a1) % Ngrid
-	
-	return x_dat, y_dat, z_dat, vx_datnew, vy_datnew, vz_datnew
+	#Calculate the new velocity at a+da
+	velocities += da*f_a1*g_p
+
+	positions = (positions + da*velocities/(a_val+da)**2*f_a1)%Ngrid 
+
+	return positions, velocities
