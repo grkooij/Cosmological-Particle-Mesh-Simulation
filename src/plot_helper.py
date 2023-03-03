7aec3de2bedbf5bef5e2479c7faa48b38891528f
unknown <grkooij@gmail.com>
2023-03-03 16:51:07 +0100

    remove comments

--- src/plot_helper.py
+++ src/plot_helper.py
@@ -62,37 +62,6 @@ def plot_projection(rho, savestep, depth):
 	plt.savefig('Data/projection_density{}.png'.format(np.int32(savestep)), dpi=1200, bbox_inches='tight')
 	plt.close()
 
-# def plot_projection(rho, positions, savestep, depth):
-# 	print("Plotting projection projection_density{}.png".format(savestep))
-# 	cmap = colors.LinearSegmentedColormap.from_list("", ["black", "steelblue", "white", "yellow", "orange", "darkred"])
-# 	cmap.set_bad((0,0,0))
-
-# 	n_slices = np.int32(BOX_SIZE/depth)
-
-# 	projection = project(rho, n_slices)
-# 	particles, z = project2(projection, positions, n_slices)
-
-
-
-# 	fig, ax = plt.subplots()
-	
-# 	# ax.imshow(projection/n_slices, extent = (0,BOX_SIZE,0,BOX_SIZE), norm=LogNorm(vmin=0.2, vmax=mass*10), cmap=cmap)
-# 	# ax.scatter(particles[0], particles[1], c=projection, s=100)
-# 	plt.scatter(x=particles[0][:], y=particles[1][:], marker="s", s=0.05)
-# 	ax.set_xlabel("Mpc/h")
-# 	ax.set_ylabel("Mpc/h")
-# 	ax.xaxis.label.set_color("white")
-# 	ax.yaxis.label.set_color("white")
-# 	ax.tick_params(axis='x', colors='white', which='both')
-# 	ax.tick_params(axis='y', colors='white', which='both')
-# 	plt.setp(ax.spines.values(), color="white")
-# 	plt.setp([ax.get_xticklines(), ax.get_yticklines()], color="white")
-	
-# 	fig.patch.set_facecolor('xkcd:black')
-# 	plt.style.context('dark_background')
-# 	plt.savefig('Data/projection_density{}.png'.format(savestep), dpi=1200, bbox_inches='tight')
-# 	plt.close()
-
 @njit
 def project(rho, n_slices):
 	projection = np.zeros(np.shape(rho[0,:,:]), dtype=np.float64)
@@ -101,20 +70,3 @@ def project(rho, n_slices):
 		projection += rho[i,:,:]
 	
 	return projection
-
-@njit
-def project2(projection, positions, n_slices):
-
-	for i in range(len(positions)):
-		if not positions[2][i] > 0. and not positions[2][i] < n_slices:
-			positions[2] = np.delete(positions[2], i)
-			positions[1] = np.delete(positions[1], i)
-			positions[0] = np.delete(positions[0], i)
-	z = np.zeros(len(positions[0][:]))
-
-	for i in range(len(positions)):
-		x = np.int32(np.floor(positions[0][i]))
-		y = np.int32(np.floor(positions[1][i]))
-		z[i] = projection[x][y]
-
-	return positions, z
